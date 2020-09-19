59112a8febc59bb61d7b740047701f2822fdf48c
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-09-19 09:07:49 +0300

    Add files via upload

---
+++ Основы программирования Python/1. The beginning/ENG/Gyroscope.py
@@ -0,0 +1,7 @@
+a = input("Enter your name:")
+b = input("Enter your surname:")
+c = input("Enter your favourite animal:")
+d = input("Enter your zodiac sign:")
+print('Individual gyroscope for the user', a, b)
+print('Who were you in last life:', c)
+print('Your zodiac sign is ', d, ', so you have a delicate nature.')
