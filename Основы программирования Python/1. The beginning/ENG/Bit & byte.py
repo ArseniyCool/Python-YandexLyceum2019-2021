1344b28a38e6f7137a4de268e873edbbcf4921ca
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-09-19 09:08:21 +0300

    Add files via upload

---
+++ Основы программирования Python/1. The beginning/ENG/Bit & byte.py
@@ -0,0 +1,7 @@
+print('ENG')
+print('1 bit - minimum unit of amount of information.')
+print('1 byte = 8 bits.')
+print('1 kilobit = 1024 bits.')
+print('1 kilobyte = 1024 bytes.')
+X = 8 * 1024
+print('1 kilobyte =', X, 'bits.')
