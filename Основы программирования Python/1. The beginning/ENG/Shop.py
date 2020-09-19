46373e420239b8376bc4384f026ea3b3736fcf8b
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-09-19 09:06:57 +0300

    Add files via upload

---
+++ Основы программирования Python/1. The beginning/ENG/Shop.py
@@ -0,0 +1,4 @@
+price = int(input('Enter the amount of the first order:'))
+price2 = int(input('Enter the amount of the second order:'))
+price_all = price + price2
+print('There is their total amount:', price_all)
