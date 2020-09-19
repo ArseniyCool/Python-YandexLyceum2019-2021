5b65b57889012af2ab26f17fc4a482f0a2af9540
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-09-19 09:09:15 +0300

    Add files via upload

---
+++ Основы программирования Python/1. The beginning/ENG/Ticket office.py
@@ -0,0 +1,5 @@
+# The program reserves movie spots at the direction of the user
+a = input('Enter the name of the film:')
+b = input('Enter the name of the cinema or shopping mall:')
+c = input('Enter session time:')
+print('Ticket on "', a, '" in "', b, '" at', c, 'booked.')
