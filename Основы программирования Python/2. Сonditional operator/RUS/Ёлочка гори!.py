cc77532b2a17bec626f937889bf357eacb0060c3
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-10-25 15:18:29 +0300

    Add files via upload

---
+++ Основы программирования Python/2. Сonditional operator/RUS/Ёлочка гори!.py
@@ -0,0 +1,9 @@
+a = input()
+b = input()
+c = input()
+if ((a == 'раз') or (a == 'один')) and (b == 'два') and (c == 'три'):
+    print("ЁЛОЧКА ГОРИ!")
+elif (a == '1') and (b == '2') and (c == '3'):
+    print("ЁЛОЧКА ГОРИ!")
+else:
+    print("ЁЛОЧКА НЕ ГОРИ...")
