15bfccd4314c5ec174cfae97c590d607bf54f263
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-10-28 22:59:18 +0300

    Add files via upload

---
+++ Основы программирования Python/4. While-loop/Таких берут в космонавты — трасировка.py
@@ -0,0 +1,23 @@
+# На профессию космонавта принимаются только люди ростом от 150 до 190 сантиметров
+# Программа считывает сколько добровольцев из данных годятся в космонавты,а также
+# Минимальный и максимальный рост участников
+test = input()
+count = 0
+test_min = 190
+test_max = 150
+while test != "!":
+    print(count)
+    print(test_min, test_max)
+    test = int(test)
+    # Cигнал остановки - !
+    if 150 < test < 190:
+        count += 1
+        if test_min > test:
+            test_min = test
+        if test_max < test:
+            test_max = test
+    print(count)
+    print(test_min, test_max)
+    test = input()
+print(count)
+print(test_min, test_max)
