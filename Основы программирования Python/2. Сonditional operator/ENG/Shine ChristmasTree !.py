c2ea26beed97b82819c41a38795e4f4214c64253
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-11-06 23:52:34 +0300

    Add files via upload

---
+++ Основы программирования Python/2. Сonditional operator/ENG/Shine ChristmasTree !.py
@@ -0,0 +1,9 @@
+a = input()
+b = input()
+c = input()
+if ((a == 'once') or (a == 'one')) and (b == 'two') and (c == 'three'):
+    print("SHINE CHRISTMAS TREE!")
+elif (a == '1') and (b == '2') and (c == '3'):
+    print("SHINE CHRISTMAS TREE!")
+else:
+    print("DON'T SHINE CHRISTMAS TREE...")
