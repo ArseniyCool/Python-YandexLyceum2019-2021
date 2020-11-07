52d2dc3d32639ddba012a90a684fc1e8f108b7ca
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-11-07 23:38:56 +0300

    Add files via upload

---
+++ Основы программирования Python/3. Python-simple-functions/ENG/Make up the bisiness card.py
@@ -0,0 +1,6 @@
+# To make a card with the name of this or that person you need a template that
+# would suit any client, for this:
+# This program displays the number of passes that must be left for a person’s name
+the_longest_name = len(input('Enter the longest name, what you know:'))
+the_longest_name = the_longest_name * 2 + 3
+print('Make', the_longest_name, 'passes in your bisiness card.')
