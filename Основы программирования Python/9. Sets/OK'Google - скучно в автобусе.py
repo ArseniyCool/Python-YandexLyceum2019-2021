8e154bedd4c9ca458bfad40dafac4a9a7104f146
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-11-13 22:10:22 +0300

    Add files via upload

---
+++ Основы программирования Python/9. Sets/OK'Google - скучно в автобусе.py
@@ -0,0 +1,23 @@
+# Игра:
+#  Вы едете на автобусе и, чтобы развеять скуку, придумали себе игру.
+#  Вы смотрите в окно и записывает все номера домов на тетрадный листок.
+#  На середине пути Вы решаете взять новый листок и продолжаете записывать номера.
+
+# Программа выводит, какие номера встретились и в первом, и во втором листочке
+first_list = set()
+second_list = set()
+third_list = set()
+for i in range(2):
+    paper = input('')
+    while paper != '':
+        if i == 1:
+            first_list.add(paper)
+        else:
+            second_list.add(paper)
+        paper = input('')
+third_list = first_list.intersection(second_list)
+if third_list == set():
+    print('Таких нет')
+else:
+    for elem in third_list:
+        print(elem)
