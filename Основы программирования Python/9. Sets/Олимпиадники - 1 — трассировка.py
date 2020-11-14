42124e19c49b4954feda185e0d5735f64205c934
Arseniy <46712495+ArseniyCool@users.noreply.github.com>
2020-11-14 23:24:59 +0300

    Add files via upload

---
+++ Основы программирования Python/9. Sets/Олимпиадники - 1 — трассировка.py
@@ -0,0 +1,34 @@
+# В школе проходит экзамен по немецкому и английскому языкам. Каждый ученик в школе
+# сдаёт либо английский, либо немецкий, либо оба этих языка.
+# У директора есть списки учеников и их фамилии изучающих каждый из языков.
+# Вот только, к несчастью,списки учеников перемешались...
+
+# Программа выводит, сколько учеников изучает только один язык.
+english = int(input('Введите кол-во учеников, сдающих английский:'))
+german = int(input('Введите кол-во учеников, сдающих немецкий:'))
+crossing = set()
+student_e_all = set()
+student_g_all = set()
+student_g = student_e = ''
+print('Вводите фамилии учеников не в обязательном порядке:')
+for i in range(english):
+    student_e = input()
+    if student_e in student_e_all:
+        student_g_all.add(student_e)
+    else:
+        student_e_all.add(student_e)
+    print('---', student_e_all)
+    print('---', student_g_all)
+for i in range(german):
+    student_g = input()
+    if student_g in student_g_all:
+        student_e_all.add(student_g)
+    else:
+        student_g_all.add(student_g)
+    print('---', student_e_all)
+    print('---', student_g_all)
+crossing = len(student_e_all ^ student_g_all)
+if crossing == 0:
+    print('Таких учеников нет')
+else:
+    print(crossing)
